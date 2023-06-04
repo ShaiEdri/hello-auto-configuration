5e2695b0f169aba371e42a83a66d68cdce11b394
shaiedri <shaiedri850@gmail.com>
2023-06-05 00:10:39 +0300

    defaultServiceIsAutoConfigured (fail) Closes #7

--- hello-starter/src/test/java/com/example/HelloAutoConfigurationTest.java
+++ hello-starter/src/test/java/com/example/HelloAutoConfigurationTest.java
@@ -1,12 +1,30 @@
 package com.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith({OutputCaptureExtension.class, SpringExtension.class})
 class HelloAutoConfigurationTest {
+    ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(HelloAutoConfiguration.class));
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    public void defaultServiceIsAutoConfigured(CapturedOutput output){
+        contextRunner.run(context -> {
+            assertEquals(1, context.getBeansOfType(HelloService.class).size());
+            context.getBean(HelloService.class).sayHello("shai");
+        });
     }
 }
