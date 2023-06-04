40d948e50283fe06f691157606b4d9486be94b04
shaiedri <shaiedri850@gmail.com>
2023-06-04 23:46:16 +0300

    Add hello-starter module and fix dependencies HelloAutoConfiguration and test for it Closes #5 Closes #6

---
+++ hello-starter/src/test/java/com/example/HelloAutoConfigurationTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloAutoConfigurationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
